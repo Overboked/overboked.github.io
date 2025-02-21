3e98d3cdf1126da0884538491b0f9eddbb0dd8db
Overboked <Overboked@gmail.com>
2025-02-21 14:38:11 +0300

    minor adjustments

--- blogData.js
+++ blogData.js
@@ -1,4 +1,4 @@
-const BLOG_PATH = 'blog-posts/'; // Путь к изображениям или файлам блогов
+const BLOG_PATH = 'blog-posts/';
 
 const blogPosts = [
     {
@@ -7,7 +7,7 @@ const blogPosts = [
         date: '2023-10-15',
         content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
         category: 'Art',
-        image: `images/art/daylight.webp`, // Изображение поста
+        image: `images/art/daylight.webp`,
         author: 'IIL Workshop'
     },
     {
@@ -19,5 +19,5 @@ const blogPosts = [
         image: `images/art/baked.webp`,
         author: 'IIL Workshop'
     }
-    // Добавь больше постов по необходимости
+   
 ];
